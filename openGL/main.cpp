78a09970ddafabfb5356d2677f6cef46eb7bd6b3
Jina <jina@Jinaui-MacBook-Pro.local>
2015-05-13 02:58:39 +0900

    Initial Commit

---
+++ openGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL
+//
+//  Created by Jina on 2015. 5. 13..
+//  Copyright (c) 2015년 Jina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
